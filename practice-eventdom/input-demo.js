3e1a702e83f55ae970ad083c7ab9080681b15f63
Sato Haru <258449@st.takushoku-u.ac.jp>
2026-05-14 15:21:16 +0900

    編集

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -2,7 +2,7 @@ function greeting() {
   console.log('こんにちは');
 }
 undefined
-b = document.querySelector('button#print');
+let b = document.querySelector('button#print');
 <button id="print">
 
 b.addEventListener('click',greeting); 
